e10ca0e9e47833e585ae95c980ad49512b32b26b
SpiffyTheSpaceman <jameskim056@gmail.com>
2020-01-16 16:41:27 -0800

    Create homepage first design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import Logo from '../../assets/images/PokemonLogo.svg';
+import Beer from '../../assets/images/Beer.svg';
+import Wave from '../../components/Wave/Wave';
+import Bubbles from '../../components/Bubbles/Bubbles';
 
 
 export default function Home(props) {
@@ -8,7 +12,16 @@ export default function Home(props) {
 
    return(
       <div className="Home-background">
-         
+         <img 
+            src={Logo} 
+            alt="" 
+            className="Home-logo"
+            />;
+         <p className="Home-subtitle">Drinking Game</p>
+         <Wave />
+         <Bubbles />
+         <img src={Beer} className="beer" alt=""/>
+         <img src={Beer} className="beer1" alt=""/>
 
       </div>
    )
